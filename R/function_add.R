8d400521274dc77ad27315843a96e050d8687e46
LaurentDV <laurent.dev.1@gmail.com>
2019-05-03 11:20:28 -0400

    test

---
+++ R/function_add.R
@@ -0,0 +1,19 @@
+
+
+#' Addition
+#'
+#' This is a function that performs an addition between two numbers.
+#'
+#' @param x A number.
+#' @param y A number to be added to the first number.
+#'
+#' @return The sum of the two numbers.
+#' @export
+#'
+#' @examples
+#'
+#' add(1,2)
+#'
+add = function(x, y) {
+  x + y
+}
